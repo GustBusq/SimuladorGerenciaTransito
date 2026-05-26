8bcfee0d78d208697adf2d1478c02a4c5a8b8153
Coordenador Cecon <coordenador.cecon@jcgestaoderiscos.com.br>
2026-05-26 11:31:44 -0300

    Implementado carregamento de ficheiros e consultas LINQ avançadas

---
+++ GerenciadorDados.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciaTransito;
+
+internal class GerenciadorDados
+{
+    // vamos ler a lista do tipo IVeiculo, para realizar o cadastro no sitema
+    public List<IVeiculo> CarregarVeiculos(string caminhoArquivo)
+    {
+        List<IVeiculo> listaCriada = new List<IVeiculo>();
+        // 1. Verificar antes de tudo se o arquivo existe
+        if (!File.Exists(caminhoArquivo))
+        {
+            Console.WriteLine("Arquivo não encontrado.");
+            return listaCriada; // Retorna uma lista vazia
+        }
+
+        // 2. Abrir o arquivo e ler linha a linha
+        using(var leitor = new StreamReader(caminhoArquivo))
+        {
+            // aqui iremos ler linha a linha do nosso arquivo enquanto ele não for null
+            string? linha;
+            while ((linha = leitor.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(linha)) continue;
+                //Para blindar o teu leitor contra utilizadores que digitam espaços sem querer utilizei trim
+                // Vamos agora separar os dados usando um delimitador
+                var partes = linha.Split(',');
+
+                var tipo = partes[0].Trim();
+                var numero = partes[1].Trim();
+                // aqui convertemos esse pedaço para inteiro.
+                var capacidade = int.Parse(partes[2]);
+
+                if (tipo == "Onibus")
+                {
+                    listaCriada.Add(new Onibus(numero, capacidade));
+                }
+                else if (tipo == "Metro")
+                {
+                    listaCriada.Add(new Metro(numero, capacidade));
+                }
+                else
+                {
+                    Console.WriteLine("Não identificado o tipo");
+                }
+            }
+        }
+        return listaCriada;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,39 +1,28 @@
 ﻿using GerenciaTransito;
 
-var listaDeVeiculos = new List<IVeiculo>()
-{
-    new Onibus("001", 25),
-    new Onibus("002", 26),
-    new Onibus("003", 28),
-    new Onibus("004", 22),
-    new Metro("005", 320),
-    new Metro("006", 320),
-    new Metro("007", 320),
-    new Metro("008", 320),
-};
+var gerenciador = new GerenciadorDados();
+var listaDeVeiculos = gerenciador.CarregarVeiculos("veiculos_cadastrados.txt");
 
-var onibus1 = listaDeVeiculos[0];
-var onibus2 = listaDeVeiculos[1];
-var onibus3 = listaDeVeiculos[2];
-var onibus4 = listaDeVeiculos[3];
-var metro1 = listaDeVeiculos[4];
-var metro2 = listaDeVeiculos[5];
-var metro3 = listaDeVeiculos[6];
-var metro4 = listaDeVeiculos[7];
-
-onibus1.MudarStatus(StatusVeiculo.EmMovimento);
-onibus2.MudarStatus(StatusVeiculo.Manutencao);
-onibus3.MudarStatus(StatusVeiculo.EmMovimento);
-onibus4.MudarStatus(StatusVeiculo.EmMovimento);
-metro1.MudarStatus(StatusVeiculo.Parado);
-metro2.MudarStatus(StatusVeiculo.EmMovimento);
-metro3.MudarStatus(StatusVeiculo.Parado);
-metro4.MudarStatus(StatusVeiculo.EmMovimento);
-
+listaDeVeiculos[0].MudarStatus(StatusVeiculo.EmMovimento);
+listaDeVeiculos[1].MudarStatus(StatusVeiculo.EmMovimento);
+listaDeVeiculos[2].MudarStatus(StatusVeiculo.EmMovimento);
 
 var veiculosEmMovimento = listaDeVeiculos.Where(o => o.Status == StatusVeiculo.EmMovimento).ToList();
 
 Relatorio relatorio = new Relatorio();
+
+var metrosExistente = listaDeVeiculos.Count(o => o.GetType().Name == "Metro");
+Console.WriteLine($"Numero de Metros existentes: {metrosExistente}");
+
+int capacidadeTotalDeOnibus = listaDeVeiculos.Where( o => o.GetType().Name == "Onibus").Sum(c => c.Capacidade);
+Console.WriteLine($"Capacidade de passageiros, somadas de todos os Onibus: {capacidadeTotalDeOnibus}");
+
+var transportesQueMaisCarrega = listaDeVeiculos.OrderByDescending(t => t.Capacidade).ToList();
+
+foreach(var t in transportesQueMaisCarrega)
+{
+    Console.WriteLine($"Veiculo: {t.NumeroDaLinha} - Capicidade: {t.Capacidade}");
+}
 
 try
 {
